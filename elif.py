b39a1881f29b1688e26b646cdc76bf8bfd6115dc
pranali1226 <163344522+pranaliSawadh@users.noreply.github.com>
2024-07-25 20:49:04 +0530

    adding elif.py

---
+++ elif.py
@@ -0,0 +1,10 @@
+score = int(input("Enter your score\n"))
+
+if (score=>90) :
+	print("your grade is A")
+elif(90>score=>75) : 
+	print("your grade is B")
+elif(75>score=>40):
+	print("your grade is C")
+else:
+  print("You are fail")
